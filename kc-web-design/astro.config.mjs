d9ffd0fa5db931abe039afb6688aebc4e5733a54
Ken Chase <kenchase@kenchase.com>
2025-06-18 11:38:48 -0400

    fix canonical URLS to use trailing slashes

--- kc-web-design/astro.config.mjs
+++ kc-web-design/astro.config.mjs
@@ -8,5 +8,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://web.kenchase.com',
+  trailingSlash: 'always',
   integrations: [mdx(), sitemap()],
 });
